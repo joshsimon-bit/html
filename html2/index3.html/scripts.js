32472cef0ab38fb7259c861e46b07afb715843d4
Joshua Simon <ga.simonj@gmail.com>
2021-06-02 15:25:50 -0400

    first commit

---
+++ html2/index3.html/scripts.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const title = document.querySelector('#title')
+const subtitle= document.querySelector(".subtitle');
+const firstParagraph = document.querySelector('p');
+const allParagraphs = document.querySelectorAll('p')
+const ClickMeButton = document.querySelection('#clickmebutton')
+
+const h#element = document.createElement
+
+rootElement.append(h3element);
+
+
+title.innerText = "Hello JavaScript!";
+ClickMeButton.addEventListener('click'), function,() {
+    alert('I HAVE BEEN CLICKED!!!');
+}
